d812ed64ebef603393fddda4aec6f8f27a70da4d
jamu-bu <jamu@bu.edu>
2023-10-12 22:01:01 -0700

    added starter code, server.js, routes

---
+++ Develop/routes/notes.js
@@ -0,0 +1,57 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+notes.get('/notes',(req, res) => {
+    readFromFile('./db/db.json').then((data) =>
+      res.json(JSON.parse(data))
+    );
+  });
+
+notes.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
+notes.delete('/:note_id', (req, res)=>{
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id !== noteId);
+            writeToFile('./db/db.json', result);
+            res.json(`Item ${noteId} has been deleted 🗑️`);
+        });
+});
+
+notes.post('/notes', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
+
+module.exports = notes;
